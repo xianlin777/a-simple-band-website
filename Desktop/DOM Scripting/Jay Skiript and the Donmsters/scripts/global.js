4dbbc8b212a11b4cbfd9050980076f8717244ea0
ZhaoGang <justinenterprising@gmail.com>
2017-03-28 11:08:24 +0800

    add functions for live.html

--- Desktop/DOM Scripting/Jay Skiript and the Donmsters/scripts/global.js
+++ Desktop/DOM Scripting/Jay Skiript and the Donmsters/scripts/global.js
@@ -264,6 +264,89 @@ addLoadEvent(prepareInternalnav);
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
 
+/live
+function stripTables() {
+  if(!document.getElementsByTagName) return false;
+  var tables = document.getElementsByTagName('table');
+  for(var i=0; i<tables.length;i++) {
+    var odd=false;
+    var rows= tables[i].getElementsByTagName('tr');
+    for(var j=0; j<rows.length;j++) {
+      if(odd == true) {
+        addClass(rows[j], 'odd');
+        odd = false;
+      } else {
+        odd = true;
+      }
+    }
+  }
+}
+
+function highlightRows() {
+  if(!document.getElementsByTagName) return false;
+  var rows = document.getElementsByTagName("tr");
+  for(var i=0; i<rows.length;i++) {
+    rows[i].oldClassName = rows[i].className;
+    rows[i].onmouseover = function() {
+      addClass(this, "highlight");
+    }
+    rows[i].onmouseout = function() {
+      this.className = this.oldClassName;
+    }
+  }
+}
+
+function displayAbbrevations() {
+  if(!document.createTextNode) return false;
+  if(!document.getElementsByTagName) return false;
+  if(!document.createElement) return false;
+
+  var abbrevations = document.getElementsByTagName("abbr");
+  if(abbrevations.length < 1) return false;
+  var defs = new Array();
+
+  for(var i = 0; i < abbrevations.length; i++) {
+    var current_abbr = abbrevations[i];
+    var defination = current_abbr.getAttribute('title');
+    var key = current_abbr.lastChild.nodeValue;
+    defs[key]=defination;
+  }
+
+  var dlist = document.createElement("dl");
+  for(key in defs) {
+    var defination = defs[key];
+    var dtitle = document.createElement("dt");
+    var dtitle_text = document.createTextNode(key);
+    dtitle.appendChild(dtitle_text);
+
+    var ddesc = document.createElement("dd");
+    var ddesc_text = document.createTextNode(defination);
+    ddesc.appendChild(ddesc_text);
+
+    dlist.appendChild(dtitle);
+    dlist.appendChild(ddesc);
+
+  }
+  if (dlist.childNodes.length < 1) return false;
+  var header = document.createElement("h3");
+  var header_text = document.createTextNode("Abbreviations");
+  header.appendChild(header_text);
+  var articles = document.getElementsByTagName("article");
+  if (articles.length == 0) return false;
+  articles[0].appendChild(header);
+  articles[0].appendChild(dlist);
+
+
+}
+
+addLoadEvent(highlightPage);
+addLoadEvent(prepareSlideshow);
+addLoadEvent(prepareInternalnav);
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+addLoadEvent(stripTables);
+addLoadEvent(highlightRows);
+addLoadEvent(displayAbbrevations);
 
 
 
